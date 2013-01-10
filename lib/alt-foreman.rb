11db0897589b80b81b0446e78ce16646107c4928
Geoff Youngs <git@intersect-uk.co.uk>
2013-01-10 15:39:36 +0000

    Add re-spawn support and old server detection

--- lib/alt-foreman.rb
+++ lib/alt-foreman.rb
@@ -7,13 +7,17 @@ module Alt
     @mutex = Mutex.new
     @senders = {}
     @colours = [:blue, :red, :yellow, :cyan, :green]
-    def self.log(sender, line)
+    def self.log(sender, line, line_colour = nil)
       @mutex.synchronize do
         colour = @senders[sender]
         unless colour
           colour = @senders[sender] = @colours[@senders.size % @colours.size]
         end
-        puts Term::ANSIColor.send(colour, Term::ANSIColor.bold(sprintf '%s %15s | ', Time.now.strftime('%H:%M:%S'), sender) + line.chomp)
+        line = line.chomp
+        if line_colour
+          line = Term::ANSIColor.send(line_colour, line)
+        end
+        puts Term::ANSIColor.send(colour, Term::ANSIColor.bold(sprintf '%s %15s | ', Time.now.strftime('%H:%M:%S'), sender) + line)
       end
     end
   end
--- lib/alt-foreman/process.rb
+++ lib/alt-foreman/process.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 module Alt
   module Foreman
     class Process
@@ -5,11 +7,54 @@ module Alt
       def initialize(name, cmd)
         @name, @cmd = name, cmd
       end
+      def log(info, col = nil)
+        Alt::Foreman.log(@name, info, col)
+      end
       def run
+        args = Shellwords.split(@cmd)
+        args.each_with_index do |arg, index|
+          if arg =~ /-p(\d+)/
+            Foreman.kill_tcp_server!($1, @name)
+          elsif arg == '-p' && args[index+1] =~ /\d+/
+            Foreman.kill_tcp_server!(args[index+1], @name)
+          end
+        end
+
         IO.popen(@cmd, 'rb') do |io|
           while line = io.gets
-            Alt::Foreman.log(@name, line)
+            log(line)
           end
+        end
+      end
+    end
+
+
+    module_function
+    def kill_tcp_server!(need_port, name)
+      tries = 0
+      loop do
+        found = false
+
+        `netstat -nWlp 2>/dev/null|grep '^tcp '`.each_line do |line|
+          _, _, _, addr, _, what, program = line.strip.split(/\s+/)
+          ip, port = addr.split(%r/:/)
+          pid, process = program.split(%r:/:)
+
+          if (port.to_i == need_port.to_i) && (pid.to_i > 0)
+            found = true
+            sig = (tries <= 5 ? 'TERM' : 'KILL')
+            Alt::Foreman.log(name, "Found conflicting server process - sending #{sig} to #{pid} & sleeping for 2s", :red)
+            ::Process.kill(sig, pid.to_i)
+            sleep 2
+            tries += 1
+          end
+        end
+
+        return unless found
+
+        if found && tries > 15
+          STDERR.puts "Failed to kill existing process - continuing anyway."
+          return
         end
       end
     end
